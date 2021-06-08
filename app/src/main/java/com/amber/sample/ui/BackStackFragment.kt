4ece22ad2b030e13e98df504add75d2534a11fcc
LIFIC-AMBER <amber@lific.io>
2021-06-08 10:33:37 +0900

    motion layout 예제 추가

--- app/src/main/java/com/amber/sample/ui/BackStackFragment.kt
+++ app/src/main/java/com/amber/sample/ui/BackStackFragment.kt
@@ -6,11 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.amber.sample.R
+import com.amber.sample.databinding.FragmentBackStackBinding
 
 class BackStackFragment : Fragment() {
+
+    lateinit var binding: FragmentBackStackBinding
+    var str = ""
+
+    companion object {
+        val KEY_STR = "key_str"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+            str = it.getString(KEY_STR).toString()
         }
     }
 
@@ -19,6 +29,9 @@ class BackStackFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_back_stack, container, false)
+        binding = FragmentBackStackBinding.inflate(inflater, container, false).apply {
+            tv.text = str
+        }
+        return binding.root
     }
 }
--- app/src/main/java/com/amber/sample/ui/MotionFragment.kt
+++ app/src/main/java/com/amber/sample/ui/MotionFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
 import com.amber.sample.R
+import com.amber.sample.databinding.FragmentMotionBinding
+import kotlinx.coroutines.flow.combine
 
 class MotionFragment : Fragment() {
 
@@ -18,7 +22,12 @@ class MotionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_motion, container, false)
-    }
+        val binding = FragmentMotionBinding.inflate(inflater, container, false).apply {
+            btnNext.setOnClickListener {
+                findNavController().navigate(R.id.backStackFragment2, bundleOf(BackStackFragment.KEY_STR to "Motion Next!!"))
+            }
+        }
 
+        return binding.root
+    }
 }
